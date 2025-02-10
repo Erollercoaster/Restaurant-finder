86b0d6d70a036ac63672b519faf582642109c589
Erollercoaster <erollfern@gmail.com>
2025-02-10 23:18:26 +0800

    Fixed puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
 # Set the port Puma will listen on
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT", 3000)
 
 # Specifies the environment
 environment ENV.fetch("RAILS_ENV") { "development" }
